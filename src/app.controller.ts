b32980bd2c8984de37c6976a7779f17e71e6ff65
Kulaev6324ab <s_kulaev@bk.ru>
2021-09-12 01:00:54 +0700

    added axious + auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,16 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('call-axious')
+  async getAxios() {
+    let data;
+    data = await this.appService.getHello();
+    console.log(data);
+    return data;
+  }
+
+  @Get('')
+  getHello() {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { lastValueFrom, map, Observable, take } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  constructor(private httpService: HttpService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    return 'hello';
+  }
+
+  token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2dpbiI6Imt1bGFldiIsImlhdCI6MTYzMTM4MTk1MywiZXhwIjoxNjMxNDY4MzUzfQ.1xhvvEuRHDzh4i15ECxYPOt1dj2qTekXRsRaCZ6O-LI';
+
+  async findAll(): Promise<any> {
+    return await lastValueFrom(
+      this.httpService
+        .get<AxiosResponse<any>>('http://localhost:4000/v1/api/user', {
+          headers: {
+            Authorization: 'Bearer ' + this.token,
+          },
+        })
+        .pipe(
+          take(1),
+          map((res) => res.data),
+        ),
+    );
   }
 }
